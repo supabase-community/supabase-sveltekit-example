58ee3759a3abdb66f0c805ee725ba008b5e3562c
Josh Nussbaum <joshnuss@gmail.com>
2021-10-02 06:12:18 -0400

    Updates config to use HMR_HOST env var

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,25 @@ import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
-	kit: {
-		// By default, `npm run build` will create a standard Node app.
-		// You can create optimized builds for different platforms by
-		// specifying a different adapter
-		adapter: node(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: sveltePreprocess(),
+  kit: {
+    // By default, `npm run build` will create a standard Node app.
+    // You can create optimized builds for different platforms by
+    // specifying a different adapter
+    adapter: node(),
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+
+    vite: {
+      server: {
+        hmr: {
+          clientPort: process.env.HMR_HOST ? 443 : 24678,
+          host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+        }
+      }
+    }
+  }
 };
